de45fcc672affec88d95b39fc8da3d3c888d40ad
fredostar <dev@frederic-radigoy.fr>
2019-04-11 11:34:42 +0200

    Add tests: enter 1 or 2 or 3 return 1 or 2 or Foo

---
+++ FooBarQix/src/main/java/fr/fredostar/FooBarQix.java
@@ -0,0 +1,12 @@
+package fr.fredostar;
+
+public class FooBarQix {
+    public String generate(int number) {
+
+        String result = String.valueOf(number);
+        if(number == 3)
+            return "Foo";
+
+        return result;
+    }
+}
---
+++ FooBarQix/src/test/java/fr/fredostar/FooBarQixTest.java
@@ -0,0 +1,27 @@
+package fr.fredostar;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class FooBarQixTest {
+
+    private FooBarQix fooBarQix;
+
+    @BeforeEach
+    void beforeEachTest(){
+         fooBarQix = new FooBarQix();
+    }
+
+    @Test void should1Return1(){
+        assertThat("1").isEqualTo(fooBarQix.generate(1));
+    }
+
+    @Test void should2Return2(){
+        assertThat("2").isEqualTo(fooBarQix.generate(2));
+    }
+    @Test void should3ReturnFooAndContains3(){
+        assertThat("Foo").isEqualTo(fooBarQix.generate(3));
+    }
+}
